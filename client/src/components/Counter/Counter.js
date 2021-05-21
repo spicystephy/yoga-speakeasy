fe5d369dc96535b137d874db0f413f51fb16e99e
Mark Columbus <mcolumbusua21@gmail.com>
2021-05-21 09:54:02 -0700

    counter

---
+++ client/src/components/Counter/Counter.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import './Counter.css'
+
+// By extending the React.Component class, Counter inherits functionality from it
+class Counter extends React.Component {
+  // Setting the initial state of the Counter component
+  state = {
+    count: 10
+  };
+
+//   // handleIncrement increases this.state.count by 1
+//   handleIncrement = () => {
+//     // We always use the setState method to update a component's state
+//     this.setState({ count: this.state.count + 1 });
+//   };
+
+  // handleDecrement decreases this.state.count by 1
+  handleDecrement = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  // The render method returns the JSX that should be rendered
+  render() {
+    return (
+      <div className="card text-center">
+        <div className="card-header bg-primary text-white">
+          Sign Up For Classes!
+        </div>
+        <div className="card-body">
+          <p className="card-text"> Mats Left : {this.state.count}</p>
+          {/* <Button className="btn btn-primary" onClick={this.handleIncrement}>
+            Increment
+          </Button>{" "} */}
+          <Button className="btn btn-danger" onClick={this.handleDecrement}>
+            Spots
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
