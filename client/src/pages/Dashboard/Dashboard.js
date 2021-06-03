14115fea1269923b2ba3400ebed0a86d8b9fa138
Mark Columbus <mcolumbusua21@gmail.com>
2021-06-03 11:18:09 -0700

    stuff

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -2,24 +2,28 @@ import React, { useState } from "react";
 import Hero from "../../components/Hero";
 import Navbar from "../../components/Navbar";
 import Counter from "../../components/Counter/Counter";
-import {CalendarComponent} from "@syncfusion/ej2-react-calendars";
+import { CalendarComponent } from "@syncfusion/ej2-react-calendars";
+import "./Dashboard.css"
 
 function Dashboard() {
   // const [loading, setLoading] = useState(false);
-const dateValue: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 20)
+  const dateValue =  new Date(
+    new Date().getFullYear(),
+    new Date().getMonth(),
+    20
+  );
   return (
-  <div>
-    
+    <div>
       <Navbar />
       <Hero className="hero-container">
         <div>
-        <h1 className="m-5 text-black">Yoga Speakeasy</h1>
-        <h2 className="m-2 p-3">Sign Up For Saturday's 10AM Class Now!</h2>
+          <h1 className="m-5">Yoga Speakeasy</h1>
+          <h2 className="m-2 p-3">Sign Up For Saturday's 10AM Class Now!</h2>
         </div>
-        <CalendarComponent value={dateValue}></CalendarComponent>
         <Counter />
+        <CalendarComponent value={dateValue}></CalendarComponent>
       </Hero>
-  </div>
+    </div>
   );
 }
 
